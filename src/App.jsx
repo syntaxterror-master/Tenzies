5a2446adfc913b21505bdc55e5895b442fc5c630
syntaxerror-master <bracketbender@gmail.com>
2025-07-23 12:14:35 +0300

    Implemented project design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  return(
+    <main>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same.
+         Click each die to freeze it at its current value between rolls.</p>
+
+      <div className="dice-container">
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
+        <button className="dice">1</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <button className="dice-btn">Roll</button>
+    </main>
   )
 }
-
-export default App
