5a19793a1a41422fa2b82592b568086028d40642
syntaxerror-master <bracketbender@gmail.com>
2025-07-23 12:23:11 +0300

    Implemented random numbers and dice roll function to generate random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
+import {useState} from "react"
+import Die from "./Die"
+
 
 export default function App() {
+
+const [dice, setDice] = useState(() => generateAllDice())
+
+function generateAllDice() {
+  return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+}
+
+function rollDice() {
+  setDice(generateAllDice())
+}
+
+const dieElements = dice.map(die => {
+    return <Die value={die}/>
+})
+
+
   return(
     <main>
-
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same.
          Click each die to freeze it at its current value between rolls.</p>
 
       <div className="dice-container">
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
-        <button className="dice">1</button>
+        {dieElements}
       </div>
 
-      <button className="dice-btn">Roll</button>
+      <button className="dice-btn" onClick={rollDice}>Roll</button>
     </main>
   )
 }
---
+++ src/Die.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Die(props) {
+  return (
+    <button className="dice">{props.value}</button>
+  )
+}
